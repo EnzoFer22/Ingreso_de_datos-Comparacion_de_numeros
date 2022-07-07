a43bce5cf5cfce1adfc446c78d31c3e0040e8216
EnzoFer22 <89401442+EnzoFer22@users.noreply.github.com>
2022-07-07 11:00:01 -0300

    Add files via upload

---
+++ Funcion y uso externo de variable/js/Script.js
@@ -0,0 +1,22 @@
+//let  num = parseFloat (prompt("Ingrese un numero para sacar su cuadrado"));
+
+//function cuadrado(){
+//    return num * num
+//}
+//let cuadrado_de_dos = cuadrado(2)
+//document.write(cuadrado_de_dos)
+
+
+
+let mayor = parseFloat (prompt("ingrese un valor"));
+
+function mayor_n () {
+    document.write ("es mayor");
+}
+
+if (mayor => 0){
+    mayor_n()
+}
+else {
+    document.write("es menor");
+}
